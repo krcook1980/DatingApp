92fefdb8d02e8a36bd86a4e37c018a62a912fea9
Ricky Panh <ricky@dragonflyut.com>
2021-05-15 13:24:18 -0600

    indexed the user model

--- models/user.js
+++ models/user.js
@@ -7,7 +7,8 @@ const userSchema = new Schema({
 
     username: {
         type: String,
-        required: true
+        required: true,
+        tags: { type: [String], index: true }
     },
 
     password: {
@@ -18,116 +19,142 @@ const userSchema = new Schema({
     email: {
         type: mongoose.SchemaTypes.Email,
         required: true,
-        default: "email@gmail.com"
+        default: "email@gmail.com",
+        tags: { type: [String], index: true }
     },
 
     age: {
         type: Number,
         default: 69,
-        unique: true
+        unique: true,
+        tags: { type: [String], index: true }
     },
 
     firstName: {
         type: String,
         required: true,
-        default: "John"
+        default: "John",
+        tags: { type: [String], index: true }
     },
 
     lastName: {
         type: String,
         required: true,
-        default: "Smith"
+        default: "Smith",
+        tags: { type: [String], index: true }
     },
 
     gender: {
         type: String,
         required: true,
+        tags: { type: [String], index: true }
     },
 
     surveyInfo: [{
 
         vacation: {
             type: String,
+            tags: { type: [String], index: true }
         },
 
         animals: {
             type: String,
+            tags: { type: [String], index: true }
         },
 
         flavor: {
             type: String,
+            tags: { type: [String], index: true }
         },
 
         activity: {
             type: String,
+            tags: { type: [String], index: true }
         },
 
         personality: {
             type: String,
+            tags: { type: [String], index: true }
         },
 
         family: {
             type: String,
+            tags: { type: [String], index: true }
         },
 
         priorities: {
             type: String,
+            tags: { type: [String], index: true }
         },
 
         entertainment: {
             type: String,
+            tags: { type: [String], index: true }
         },
 
         alcohol: {
             type: String,
+            tags: { type: [String], index: true }
         },
 
         religion: {
             type: String,
+            tags: { type: [String], index: true }
         },
 
         biking: {
             type: Boolean,
+            tags: { type: [Boolean], index: true }
         },
 
         camping: {
             type: Boolean,
+            tags: { type: [Boolean], index: true }
         },
 
         computers: {
             type: Boolean,
+            tags: { type: [Boolean], index: true }
         },
 
         cooking: {
             type: Boolean,
+            tags: { type: [Boolean], index: true }
         },
 
         dadJokes: {
             type: Boolean,
+            tags: { type: [Boolean], index: true }
         },
 
         exercise: {
             type: Boolean,
+            tags: { type: [Boolean], index: true }
         },
 
         fishing: {
             type: Boolean,
+            tags: { type: [Boolean], index: true }
         },
 
         gaming: {
             type: Boolean,
+            tags: { type: [Boolean], index: true }
         },
 
         hiking: {
             type: Boolean,
+            tags: { type: [Boolean], index: true }
         },
 
         reading: {
             type: Boolean,
+            tags: { type: [Boolean], index: true }
         },
 
         techDrones: {
             type: Boolean,
+            tags: { type: [Boolean], index: true }
         },
     }],
 
@@ -135,8 +162,11 @@ const userSchema = new Schema({
     blockedUsers: [{}],
 
 
-
 });
+
+userSchema.index({
+
+})
 
 const User = mongoose.model("User", userSchema);
 
