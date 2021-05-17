ddb7e571fdf2a53985097d4ae45a0d391192ea68
Andrew Burke <Drewburke39@gmail.com>
2021-05-17 16:05:14 -0600

    Login and Signup pages are rendering

--- client/src/App.js
+++ client/src/App.js
@@ -6,26 +6,31 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ContactsProvider } from "./contexts/contactsProvider";
 import { ConversationProvider } from './contexts/conversationProvider';
 import { SocketProvider } from './contexts/SocketProvider';
-
+import SignupForm from './pages/SignupForm'
 
 {/* <Login onIdSubmit={setId} onPwdSubmit={setPassword} /> */ }
 function App() {
-  const [ID, setId] = useState();
-  const id = "Crazy"
-  const [password, setPassword] = useState();
-  const dashboard = (
-    <SocketProvider id={id}>
-      <ContactsProvider>
-        <ConversationProvider id={id}>
-          <Dashboard id={id} />
-        </ConversationProvider>
-      </ContactsProvider>
-    </SocketProvider >
-  )
+  // const [ID, setId] = useState();
+  // const id = "Crazy"
+  // const [password, setPassword] = useState();
+  // const dashboard = (
+  //   <SocketProvider id={id}>
+  //     <ContactsProvider>
+  //       <ConversationProvider id={id}>
+  //         <Dashboard id={id} />
+  //       </ConversationProvider>
+  //     </ContactsProvider>
+  //   </SocketProvider >
+  // )
 
   return (
-    id ? dashboard : <Login onSubmit={setId} />
-
+    // id ? dashboard : <Login onSubmit={setId} />
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/SignupForm" component={SignupForm} />
+      </Switch>
+    </Router>
   );
 }
 
