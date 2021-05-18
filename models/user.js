6809bd1c1528a3a935225355a7767416d3946fe6
Ricky Panh <ricky@dragonflyut.com>
2021-05-17 20:55:23 -0600

    fixed user model, because i'm dumb

--- models/user.js
+++ models/user.js
@@ -26,7 +26,6 @@ const userSchema = new Schema({
     age: {
         type: Number,
         default: 69,
-        unique: true,
         tags: { type: [String], index: true }
     },
 
@@ -50,116 +49,113 @@ const userSchema = new Schema({
         tags: { type: [String], index: true }
     },
 
-    surveyInfo: [{
+    vacation: {
+        type: String,
+        tags: { type: [String], index: true }
+    },
 
-        vacation: {
-            type: String,
-            tags: { type: [String], index: true }
-        },
+    animals: {
+        type: String,
+        tags: { type: [String], index: true }
+    },
 
-        animals: {
-            type: String,
-            tags: { type: [String], index: true }
-        },
+    flavor: {
+        type: String,
+        tags: { type: [String], index: true }
+    },
 
-        flavor: {
-            type: String,
-            tags: { type: [String], index: true }
-        },
+    activity: {
+        type: String,
+        tags: { type: [String], index: true }
+    },
 
-        activity: {
-            type: String,
-            tags: { type: [String], index: true }
-        },
+    personality: {
+        type: String,
+        tags: { type: [String], index: true }
+    },
 
-        personality: {
-            type: String,
-            tags: { type: [String], index: true }
-        },
+    family: {
+        type: String,
+        tags: { type: [String], index: true }
+    },
 
-        family: {
-            type: String,
-            tags: { type: [String], index: true }
-        },
+    priorities: {
+        type: String,
+        tags: { type: [String], index: true }
+    },
 
-        priorities: {
-            type: String,
-            tags: { type: [String], index: true }
-        },
+    entertainment: {
+        type: String,
+        tags: { type: [String], index: true }
+    },
 
-        entertainment: {
-            type: String,
-            tags: { type: [String], index: true }
-        },
+    alcohol: {
+        type: String,
+        tags: { type: [String], index: true }
+    },
 
-        alcohol: {
-            type: String,
-            tags: { type: [String], index: true }
-        },
+    religion: {
+        type: String,
+        tags: { type: [String], index: true }
+    },
 
-        religion: {
-            type: String,
-            tags: { type: [String], index: true }
-        },
+    biking: {
+        type: Boolean,
+        tags: { type: [Boolean], index: true }
+    },
 
-        biking: {
-            type: Boolean,
-            tags: { type: [Boolean], index: true }
-        },
+    camping: {
+        type: Boolean,
+        tags: { type: [Boolean], index: true }
+    },
 
-        camping: {
-            type: Boolean,
-            tags: { type: [Boolean], index: true }
-        },
+    computers: {
+        type: Boolean,
+        tags: { type: [Boolean], index: true }
+    },
 
-        computers: {
-            type: Boolean,
-            tags: { type: [Boolean], index: true }
-        },
+    cooking: {
+        type: Boolean,
+        tags: { type: [Boolean], index: true }
+    },
 
-        cooking: {
-            type: Boolean,
-            tags: { type: [Boolean], index: true }
-        },
+    dadJokes: {
+        type: Boolean,
+        tags: { type: [Boolean], index: true }
+    },
 
-        dadJokes: {
-            type: Boolean,
-            tags: { type: [Boolean], index: true }
-        },
+    exercise: {
+        type: Boolean,
+        tags: { type: [Boolean], index: true }
+    },
 
-        exercise: {
-            type: Boolean,
-            tags: { type: [Boolean], index: true }
-        },
+    fishing: {
+        type: Boolean,
+        tags: { type: [Boolean], index: true }
+    },
 
-        fishing: {
-            type: Boolean,
-            tags: { type: [Boolean], index: true }
-        },
+    gaming: {
+        type: Boolean,
+        tags: { type: [Boolean], index: true }
+    },
 
-        gaming: {
-            type: Boolean,
-            tags: { type: [Boolean], index: true }
-        },
+    hiking: {
+        type: Boolean,
+        tags: { type: [Boolean], index: true }
+    },
 
-        hiking: {
-            type: Boolean,
-            tags: { type: [Boolean], index: true }
-        },
+    reading: {
+        type: Boolean,
+        tags: { type: [Boolean], index: true }
+    },
 
-        reading: {
-            type: Boolean,
-            tags: { type: [Boolean], index: true }
-        },
+    techDrones: {
+        type: Boolean,
+        tags: { type: [Boolean], index: true }
+    },
 
-        techDrones: {
-            type: Boolean,
-            tags: { type: [Boolean], index: true }
-        },
-    }],
-
-    myConnections: [{}],
-    blockedUsers: [{}],
+    myConnections: Array,
+    blockedUsers: Array,
 
 
 });
